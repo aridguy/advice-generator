c9507b4d30ae861a0f2f7c3c2669755472047c11
aridguy <50306833+aridguy@users.noreply.github.com>
2023-11-03 15:25:00 +0100

    done with advice slip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+  const [nextAdvice, setNextAdvice] = useState(0)
+  useEffect(() => {
+    axios.get("https://api.adviceslip.com/advice")
+    .then((res) => setAdvice(res.data.slip))
+    .catch((error)=> {console.log(error);})
+   }, [nextAdvice]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6">
+              <div className="adv-bg ">
+                <div className="adc-contents">
+                  <div className="avc-head p-2 text-center">
+                    <small className="brand-two">ADVICE#{advice.id}</small>
+                  </div>
+                  <div className="adc-body text-center text-mute p-5 advice">
+                    <span>
+                      {advice.advice}
+                     
+                    </span>
+                  </div>
+                  <div className="adc-footer p-4">
+                    <div className="row">
+                      <div className="col-md-1"></div>
+                      <div className="col-md-10">
+                        <div className="d-flex gap-4 lines">
+                          <hr className="text-white" />{" "}
+                          <i className="fas fa-pause text-white"></i>{" "}
+                          <hr className="text-white" />
+                        </div>
+                        <div className="text-center adviceBtn">
+                          <i
+                            onClick={()=> setNextAdvice(nextAdvice+1)}
+                            className="fas fa-qrcode mt-5 cursor brand-two"
+                          ></i>
+                        </div>
+                      </div>
+                      <div className="col-md-1"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
